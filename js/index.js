c1c398733d6f1e66960d1d7e8c8af400b8780204
Adam Podoxin <adam.podoxin@gmail.com>
2020-03-29 17:05:54 -0700

    Created status display

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+let statusColors = new Map();
+statusColors.set("not done", "var(--status-not-done)");
+statusColors.set("in progress", "var(--status-in-progress)");
+statusColors.set("done", "var(--status-done)");
+
 function onLoad() {
     var listItems = document.getElementsByClassName("list-item");
     Array.prototype.forEach.call(listItems, function (element) {
@@ -7,6 +12,26 @@ function onLoad() {
 
 function setStatus(listItem, status) {
     listItem.setAttribute("status", status);
+    displayStatus(listItem);
+}
+
+function displayStatus(listItem) {
+    var status = listItem.getAttribute("status");
+    var color = statusColors.get(status);
+
+    var statusBar = getStatusElement(listItem);
+    statusBar.style.backgroundColor = color;
+}
+
+function getStatusElement(listItem) {
+    var statusElement = null;
+
+    Array.prototype.forEach.call(listItem.childNodes, function (element) {
+        if (element.className == "item-status")
+            statusElement = element;
+    });
+
+    return statusElement;
 }
 
 window.onload = onLoad();
