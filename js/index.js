596928a0165b1338bfa474aafaf24f66c009a590
Adam Podoxin <adam.podoxin@gmail.com>
2020-03-29 18:50:28 -0700

    Created list item editor

--- js/index.js
+++ js/index.js
@@ -3,11 +3,37 @@ statusColors.set("not done", "var(--status-not-done)");
 statusColors.set("in progress", "var(--status-in-progress)");
 statusColors.set("done", "var(--status-done)");
 
+var itemModal;
+var itemModalText;
+
+var editingListItem;
+
 function onLoad() {
+    itemModal = document.getElementById("item-modal");
+    itemModalText = document.getElementById("item-modal-text");
+
     var listItems = document.getElementsByClassName("list-item");
+
     Array.prototype.forEach.call(listItems, function (element) {
         setStatus(element, "not done");
+        setText(element, "");
+
+        element.addEventListener("click", function (onClickEvent) {
+            openItemModal(onClickEvent.target);
+        });
     });
+}
+
+function setText(listItem, text) {
+    listItem.setAttribute("text", text);
+    displayText(listItem);
+}
+
+function displayText(listItem) {
+    var text = listItem.getAttribute("text");
+
+    var textElement = getFirstChildWithClass(listItem, "item-text");
+    textElement.innerHTML = text;
 }
 
 function setStatus(listItem, status) {
@@ -19,19 +45,38 @@ function displayStatus(listItem) {
     var status = listItem.getAttribute("status");
     var color = statusColors.get(status);
 
-    var statusBar = getStatusElement(listItem);
+    var statusBar = getFirstChildWithClass(listItem, "item-status");
     statusBar.style.backgroundColor = color;
 }
 
-function getStatusElement(listItem) {
-    var statusElement = null;
+function getFirstChildWithClass(element, childClass) {
+    var child = null;
 
-    Array.prototype.forEach.call(listItem.childNodes, function (element) {
-        if (element.className == "item-status")
-            statusElement = element;
+    Array.prototype.forEach.call(element.childNodes, function (element) {
+        if (element.className == childClass)
+            child = element;
     });
 
-    return statusElement;
+    return child;
+}
+
+function openItemModal(listItem) {
+    editingListItem = listItem;
+
+    itemModal.style.display = "block";
+    itemModalText.value = listItem.getAttribute("text");
+}
+
+function closeItemModal() {
+    itemModal.style.display = "none";
+}
+
+function setTextModal() {
+    setText(editingListItem, itemModalText.value);
+}
+
+function setStatusModal(status) {
+    setStatus(editingListItem, status);
 }
 
 window.onload = onLoad();
